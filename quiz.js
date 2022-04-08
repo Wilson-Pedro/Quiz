dd8bd1de1629f490dfedac67caf24f1002337e80
Wilson-Pedro <96023861+Wilson-Pedro@users.noreply.github.com>
2022-04-08 19:44:18 -0300

    ajustes

--- quiz.js
+++ quiz.js
@@ -7,45 +7,50 @@ const numQuest = document.getElementById('numeroQuestao')
 const container = document.querySelector('.container')
 const start = document.getElementById('Start')
 const res = document.getElementById('res')
-var cont = 0
-const q = [3]
+var cont = 1
+var nQuestao = 0
+const q = [4]
 
 
 // QUESTÕES
 q[0] = {
     perguntaQuestao: 'Quanto é 1 + 1 ?',
-    alternativa1: '1',
-    alternativa2: '2',
-    alternativa3: '3',
-    alternativa4: '4',
-    correta: '2',
+    alternativa1: 1,
+    alternativa2: 2,
+    alternativa3: 3,
+    alternativa4: 4,
+    correta: 2,
 }
 
 q[1] = {
     perguntaQuestao: 'Quanto é 2 + 2 ?',
-    alternativa1: '4',
-    alternativa2: '5',
-    alternativa3: '6',
-    alternativa4: '7',
-    correta: '4',
+    alternativa1: 4,
+    alternativa2: 5,
+    alternativa3: 6,
+    alternativa4: 7,
+    correta: 4,
 }
 
 q[2] = {
     perguntaQuestao: 'Quanto é 3 + 3 ?',
-    alternativa1: '8',
-    alternativa2: '9',
-    alternativa3: '6',
-    alternativa4: '0',
-    correta: '6',
+    alternativa1: 8,
+    alternativa2: 9,
+    alternativa3: 6,
+    alternativa4: 0,
+    correta: 6,
 }
 
 q[3] = {
     perguntaQuestao: 'Quanto é 4 + 4 ?',
-    alternativa1: '7',
-    alternativa2: '2',
-    alternativa3: '3',
-    alternativa4: '8',
-    correta: '8',
+    alternativa1: 7,
+    alternativa2: 2,
+    alternativa3: 3,
+    alternativa4: 8,
+    correta: 8,
+}
+
+q[4] = {
+    perguntaQuestao: 'Quiz Finalizado!'
 }
 
 // BOTÃO PARA INICICAR O QUIZ
@@ -59,11 +64,12 @@ function iniciar() {
     numQuest.textContent =  `Pergunta 1/4`
     start.disabled = true
     start.style.display = 'none'
+    res.innerHTML = nQuestao
+    res.innerHTML += ' ' + cont
 }
 
 // FUNÇÃO PARA AVANÇAR PARA AS PRÓXIMAS QUESTÕES
 function proximaQuestao() {
-    cont += 1
     numQuest.textContent =  `Pergunta ${cont}/4`
     disabled_btn()
     pergunta.textContent = q[cont].perguntaQuestao
@@ -71,21 +77,23 @@ function proximaQuestao() {
     a2.textContent = q[cont].alternativa2
     a3.textContent = q[cont].alternativa3
     a4.textContent = q[cont].alternativa4
-    QuestaoCorreta()
+    questaoCorreta()
+    cont += 1
+    nQuestao += 1
+    res.innerHTML = nQuestao
+    res.innerHTML += ' ' + cont
 }
 
 //FUNÇÃO PARA VERFICAR A QUESTÃO CORRETA
-function QuestaoCorreta(){
-    var res = document.querySelector('#res')
-    let resposta = q1.alternativa1.value
-    res.textContent = `${resposta}`
+
+function questaoCorreta(){
     
     return true
 }
 
 // FUNÇÃO PARA DESABILITAR OS BOTES APÓS O QUIZ
 function disabled_btn(){
-    if (cont == 4){
+    if (cont == 5){
         a1.setAttribute("disabled", "disable")
         a2.setAttribute("disabled", "disable")
         a3.setAttribute("disabled", "disable")
